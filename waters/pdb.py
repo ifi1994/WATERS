c4edb7f7ac0eb5d4aab04ff48912ef3b0cc894ef
Pantelispanka <pantelispanka@gmail.com>
2024-04-05 17:57:55 +0300

    running

--- waters/pdb.py
+++ waters/pdb.py
@@ -1,35 +1,38 @@
 from Bio.PDB import PDBParser, Structure
 
 file1 = open( "/Users/pantelispanka/Downloads/conf-protein.pdb", 'r')
+
+# file1 = open("/Users/pantelispanka/Downloads/segment_4_input_af_all.pdb")
+
 Lines = file1.readlines()
 
 count = 0
 # Strips the newline character
 
-# model = None
-# new = True
-# i = 0
-# for line in Lines:
-#     if new:
-#         file = open("./" + str(i) + "_a.pdb", "w")
-#     file.write(line)
-#     new = False
-#     if "ENDMDL" in line:
-#         model = None
-#         i += 1
-#         new = True
-    # print("Line{}: {}".format(count, line.strip()))
+model = None
+new = True
+i = 0
+for line in Lines:
+    if new:
+        file = open("./a_syn/" + str(i) + "_a.pdb", "w")
+    file.write(line)
+    new = False
+    if "ENDMDL" in line:
+        model = None
+        i += 1
+        new = True
+    print("Line{}: {}".format(count, line.strip()))
 
 
 
 
-sloppyparser = PDBParser()
-structure = sloppyparser.get_structure("0", "/Users/pantelispanka/Jaqpot/WATERS/waters/1_a.pdb")
-
-
-
-# structure = sloppyparser.get_structure("0", "/Users/pantelispanka/Downloads/conf-protein.pdb")
+# sloppyparser = PDBParser()
+# structure = sloppyparser.get_structure("0", "/Users/pantelispanka/Jaqpot/WATERS/waters/1_a.pdb")
 #
 #
-for model in structure.get_models():
-    print(model)
+#
+# # structure = sloppyparser.get_structure("0", "/Users/pantelispanka/Downloads/conf-protein.pdb")
+# #
+# #
+# for model in structure.get_models():
+#     print(model)
--- waters/waters_first.py
+++ waters/waters_first.py
@@ -2,23 +2,39 @@ from rdkit import Chem
 import os
 import numpy as np
 
+hydrophobic_waters = "magenta > 0"
+hydrophilic_waters = "yellow < 0"
+
+hydrophobic_atoms = ['C', 'S', 'F']
+# hydrophilic_atoms = ['N', 'O']
+hydrophilic_atoms = ['N', 'O', 'Br', 'Cl']
+
+"hydrophobic_atoms <-> hydrophobic_waters > 0"
+"hydrophilic_waters <-> hydrophilic_waters < 0"
 
 # folder path
 dir_path = "/Users/pantelispanka/Jaqpot/WATERS/data/sdf_multiple_entries"
+
+
+# water_data = "/Users/pantelispanka/Jaqpot/WATERS/data/Perampanel_analogue_data/Water_data"
+water_data = "/Users/pantelispanka/Jaqpot/WATERS/data/Water_data"
+
+# ligand_data = "/Users/pantelispanka/Jaqpot/WATERS/data/Perampanel_analogue_data/Ligand_data"
+ligand_data = "/Users/pantelispanka/Jaqpot/WATERS/data/Ligand_data"
 
 # list to store files
 sdfs = []
 
 # Iterate directory
-for path in os.listdir(dir_path):
+for path in os.listdir(water_data):
     # check if current path is a file
-    if os.path.isfile(os.path.join(dir_path, path)):
+    if os.path.isfile(os.path.join(water_data, path)):
         sdfs.append(path)
 
 water_mols = []
 
 for file in sdfs:
-    path = dir_path + "/" + file
+    path = water_data + "/" + file
     suppl = Chem.SDMolSupplier(path)
     for mol in suppl:
         water_mols.append(mol)
@@ -124,9 +140,13 @@ for mol in water_mols:
     for i, atom in enumerate(mol.GetAtoms()):
         positions = mol.GetConformer().GetAtomPosition(i)
         # print("CENTER: x -> " + str(int(positions.x)), " , y -> " + str(int(positions.y)), " , z -> " + str(int(positions.z)))
-        x = int(round(positions.x))
-        y = int(round(positions.y))
-        z = int(round(positions.z))
+        # x = int(round(positions.x, 1))
+        # y = int(round(positions.y, 1))
+        # z = int(round(positions.z, 1))
+        x = round(positions.x, 2)
+        y = round(positions.y, 2)
+        z = round(positions.z, 2)
+
 
         # x = abs(int(positions.x) + min_x_int)
         # y = abs(int(positions.y) + min_y_int)
@@ -161,12 +181,410 @@ for key in grid:
 
 
 for key in grid:
-    if grid[key]['std'] > 0.0:
+    if grid[key]['mean'] > 0.0:
         print(grid[key]['x'])
         print(grid[key]['y'])
         print(grid[key]['z'])
         print(grid[key]['energies'])
         print(grid[key]['std'])
+
+mol_sdfs = []
+
+# Iterate directory
+for path in os.listdir(ligand_data):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(ligand_data, path)):
+        mol_sdfs.append(path)
+
+
+ligand_mols = []
+
+for file in mol_sdfs[:40]:
+    path = ligand_data + "/" + file
+    suppl = Chem.SDMolSupplier(path)
+    for mol in suppl:
+        ligand_mols.append(mol)
+
+
+
+print(len(ligand_mols))
+
+import math
+
+
+def distance(x1, y1, z1, x2, y2, z2):
+    d = 0.0
+    d = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    return d
+
+atoms_set = set()
+
+mol_maps = []
+mol_map = {}
+for ind, mol in enumerate(ligand_mols):
+    # print(mol.GetProp('szmap_neut_diff_free_energy'))
+    print(ind)
+    print(mol_sdfs[ind])
+
+    mol_map[mol_sdfs[ind]] = {}
+    mol_map[mol_sdfs[ind]]['phil_phil'] = {}
+    mol_map[mol_sdfs[ind]]['phil_phob'] = {}
+    mol_map[mol_sdfs[ind]]['phob_phob'] = {}
+    mol_map[mol_sdfs[ind]]['phob_phil'] = {}
+    mol_map[mol_sdfs[ind]]['phil_phil']['atoms'] = []
+    mol_map[mol_sdfs[ind]]['phil_phil']['grid_energies'] = []
+    mol_map[mol_sdfs[ind]]['phil_phil']['std'] = []
+    mol_map[mol_sdfs[ind]]['phil_phob']['atoms'] = []
+    mol_map[mol_sdfs[ind]]['phil_phob']['grid_energies'] = []
+    mol_map[mol_sdfs[ind]]['phil_phob']['std'] = []
+    mol_map[mol_sdfs[ind]]['phob_phob']['atoms'] = []
+    mol_map[mol_sdfs[ind]]['phob_phob']['grid_energies'] = []
+    mol_map[mol_sdfs[ind]]['phob_phob']['std'] = []
+    mol_map[mol_sdfs[ind]]['phob_phil']['atoms'] = []
+    mol_map[mol_sdfs[ind]]['phob_phil']['grid_energies'] = []
+    mol_map[mol_sdfs[ind]]['phob_phil']['std'] = []
+    for i, atom in enumerate(mol.GetAtoms()):
+        positions = mol.GetConformer().GetAtomPosition(i)
+        # print(atom.GetSymbol())
+        # print(positions.x)
+        # print(positions.y)
+        # print(positions.z)
+        atom_symbol = atom.GetSymbol()
+        atoms_set.add(atom_symbol)
+        atom_x = positions.x
+        atom_y = positions.y
+        atom_z = positions.z
+        for key in grid:
+            grid_x = grid[key]['x']
+            grid_y = grid[key]['y']
+            grid_z = grid[key]['z']
+            energy = grid[key]['energies']
+            std = grid[key]['std']
+            dist = distance(atom_x, atom_y, atom_z, grid_x, grid_y, grid_z)
+            if dist < 0.8:
+                if atom_symbol in hydrophilic_atoms:
+                    energy_mean = sum(energy) / len(energy)
+                    if -0 > energy_mean > -12:
+                        atom_in = f"{atom_symbol}_in_phil"
+                        try:
+                            mol_map[mol_sdfs[ind]]['phil_phil'][atom_in].append(1)
+                        except KeyError as e:
+                            mol_map[mol_sdfs[ind]]['phil_phil'][atom_in] = [1]
+                        mol_map[mol_sdfs[ind]]['phil_phil']['atoms'].append(atom_symbol)
+                        mol_map[mol_sdfs[ind]]['phil_phil']['grid_energies'].append(energy_mean)
+                        mol_map[mol_sdfs[ind]]['phil_phil']['std'].append(std)
+                    if energy_mean > 0:
+                        atom_in = f"{atom_symbol}_in_phob"
+                        try:
+                            mol_map[mol_sdfs[ind]]['phil_phob'][atom_in].append(1)
+                        except KeyError as e:
+                            mol_map[mol_sdfs[ind]]['phil_phob'][atom_in] = [1]
+                        mol_map[mol_sdfs[ind]]['phil_phob']['atoms'].append(atom_symbol)
+                        mol_map[mol_sdfs[ind]]['phil_phob']['grid_energies'].append(energy_mean)
+                        mol_map[mol_sdfs[ind]]['phil_phob']['std'].append(std)
+                if atom_symbol in hydrophobic_atoms:
+                    energy_mean = sum(energy) / len(energy)
+                    if -0 > energy_mean > -12:
+                        atom_in = f"{atom_symbol}_in_phil"
+                        try:
+                            mol_map[mol_sdfs[ind]]['phob_phil'][atom_in].append(1)
+                        except KeyError as e:
+                            mol_map[mol_sdfs[ind]]['phob_phil'][atom_in] = [1]
+                        mol_map[mol_sdfs[ind]]['phob_phil']['atoms'].append(atom_symbol)
+                        mol_map[mol_sdfs[ind]]['phob_phil']['grid_energies'].append(energy_mean)
+                        mol_map[mol_sdfs[ind]]['phob_phil']['std'].append(std)
+                    if energy_mean > 0:
+                        atom_in = f"{atom_symbol}_in_phob"
+                        try:
+                            mol_map[mol_sdfs[ind]]['phob_phob'][atom_in].append(1)
+                        except KeyError as e:
+                            mol_map[mol_sdfs[ind]]['phob_phob'][atom_in] = [1]
+                        mol_map[mol_sdfs[ind]]['phob_phob']['atoms'].append(atom_symbol)
+                        mol_map[mol_sdfs[ind]]['phob_phob']['grid_energies'].append(energy_mean)
+                        mol_map[mol_sdfs[ind]]['phob_phob']['std'].append(std)
+
+
+                    # print("-----FOUND CLOSE HYDROPHIL----")
+                    # print(atom_symbol)
+                    # print("----THE ENERGY----")
+                    # energy_mean = sum(energy) / len(energy)
+                    # print(energy_mean)
+            # if grid[key]['mean'] > 0.0:
+            #     grid_x = grid[key]['x']
+            #     grid_y = grid[key]['y']
+            #     grid_z = grid[key]['z']
+            #     energy = grid[key]['energies']
+            #     std = grid[key]['std']
+
+print(atoms_set)
+print("-------MOL MAP-----")
+print(len(mol_map))
+for mol in mol_map:
+    print(mol_map[mol])
+    print("\n")
+
+
+
+dataset = {}
+
+# dataset["7L10"] = 4.02
+dataset["7L11"] = 0.140
+dataset["7M8X"] = 0.470
+dataset["7M8M"] = 0.12
+dataset["7L12"] = 0.128
+dataset["7M8Y"] = 0.110
+dataset["7M8N"] = 0.1
+dataset["7M8O"] = 0.037
+dataset["7L13"] = 0.018
+dataset["7M8P"] = 0.02
+dataset["7M91"] = 0.025
+dataset["7L14"] = 0.170
+dataset["7M8Z"] = 0.25
+dataset["7M90"] = 0.25
+
+
+dataset_class = {}
+# dataset["7L10"] = 0
+dataset_class["7L11"] = 0
+dataset_class["7M8X"] = 0
+dataset_class["7M8M"] = 0
+dataset_class["7L12"] = 0
+dataset_class["7M8Y"] = 0
+dataset_class["7M8N"] = 1
+dataset_class["7M8O"] = 1
+dataset_class["7L13"] = 1
+dataset_class["7M8P"] = 1
+dataset_class["7M91"] = 1
+dataset_class["7L14"] = 0
+dataset_class["7M8Z"] = 0
+dataset_class["7M90"] = 0
+
+
+model_datas = []
+
+print("---PRINTING---")
+for key in mol_map:
+    model_data = {}
+    print("MOLECULE")
+    print(key)
+    molecule = key.split("-")[1].split(".")[0]
+    if molecule != "7L10":
+        print(molecule)
+        model_data['Id'] = molecule
+        print("IC50")
+        print(dataset[molecule])
+        model_data['IC50'] = dataset[molecule]
+        model_data['CLASS'] = dataset_class[molecule]
+        print(key.split("-")[1].split(".")[0])
+        print(mol_map[key])
+        print('phil_phil')
+        print(len(mol_map[key]['phil_phil']['atoms']))
+        model_data['phil_phil'] = len(mol_map[key]['phil_phil']['atoms'])
+        print('phil_phob')
+        print(len(mol_map[key]['phil_phob']['atoms']))
+        model_data['phil_phob'] = len(mol_map[key]['phil_phob']['atoms'])
+        print('phob_phob')
+        print(len(mol_map[key]['phob_phob']['atoms']))
+        model_data['phob_phob'] = len(mol_map[key]['phob_phob']['atoms'])
+        print('phob_phil')
+        print(len(mol_map[key]['phob_phil']['atoms']))
+        model_data['phob_phil'] = len(mol_map[key]['phob_phil']['atoms'])
+
+        energy = mol_map[key]['phob_phil']['grid_energies']
+        std = mol_map[key]['phob_phil']['std']
+        energies_phob_phil = mol_map[key]['phob_phil']['grid_energies']
+
+
+
+        try:
+            energies_phob_phil_mean = sum(energies_phob_phil) / len(energies_phob_phil)
+        except ZeroDivisionError as e:
+            energies_phob_phil_mean = 0
+            print(str(e))
+        print("energies_phob_phil")
+        print(energies_phob_phil_mean)
+
+        model_data['energies_phob_phil_mean'] = energies_phob_phil_mean
+
+        energy = mol_map[key]['phob_phob']['grid_energies']
+        std = mol_map[key]['phob_phob']['std']
+        energies_phob_phob = mol_map[key]['phob_phob']['grid_energies']
+        try:
+            energies_phob_phob_mean = sum(energies_phob_phob) / len(energies_phob_phob)
+        except ZeroDivisionError as e:
+            energies_phob_phob_mean = 0
+            print(str(e))
+        print("energies_phob_phob")
+        print(energies_phob_phob_mean)
+
+        model_data['energies_phob_phob_mean'] = energies_phob_phob_mean
+
+        energy = mol_map[key]['phil_phil']['grid_energies']
+        std = mol_map[key]['phil_phil']['std']
+        energies_phil_phil = mol_map[key]['phil_phil']['grid_energies']
+        try:
+            energies_phil_phil_mean = sum(energies_phil_phil) / len(energies_phil_phil)
+        except ZeroDivisionError as e:
+            energies_phil_phil_mean = 0
+            print(str(e))
+        print("energies_phil_phil")
+        print(energies_phil_phil_mean)
+
+        model_data['energies_phil_phil_mean'] = energies_phil_phil_mean
+
+        energy = mol_map[key]['phil_phob']['grid_energies']
+        std = mol_map[key]['phil_phob']['std']
+        energies_phil_phob = mol_map[key]['phil_phob']['grid_energies']
+        try:
+            energies_phil_phob_mean = sum(energies_phil_phob) / len(energies_phil_phob)
+        except ZeroDivisionError  as e:
+            energies_phil_phob_mean = 0
+            print(str(e))
+        print("energies_phil_phil")
+        print(energies_phil_phob_mean)
+
+        model_data['energies_phil_phob_mean'] = energies_phil_phob_mean
+
+        for atom in hydrophobic_atoms:
+            map = f"{atom}_in_phil"
+            map2 = f"{atom}_in_phob"
+            try:
+                val = len(mol_map[key]['phob_phob'][map])
+                model_data[map] = val
+            except KeyError as e:
+                model_data[map] = 0
+            try:
+                val = len(mol_map[key]['phob_phil'][map2])
+                model_data[map2] = val
+            except KeyError as e:
+                model_data[map2] = 0
+            try:
+                val = len(mol_map[key]['phil_phob'][map])
+                model_data[map] = val
+            except KeyError as e:
+                model_data[map] = 0
+            try:
+                val = len(mol_map[key]['phil_phil'][map2])
+                model_data[map2] = val
+            except KeyError as e:
+                model_data[map2] = 0
+
+        for atom in hydrophilic_atoms:
+            map = f"{atom}_in_phil"
+            map2 = f"{atom}_in_phob"
+            try:
+                val = len(mol_map[key]['phil_phob'][map])
+                model_data[map] = val
+            except KeyError as e:
+                model_data[map] = 0
+            try:
+                val = len(mol_map[key]['phil_phil'][map2])
+                model_data[map2] = val
+            except KeyError as e:
+                model_data[map2] = 0
+            try:
+                val = len(mol_map[key]['phil_phob'][map])
+                model_data[map] = val
+            except KeyError as e:
+                model_data[map] = 0
+            try:
+                val = len(mol_map[key]['phil_phil'][map2])
+                model_data[map2] = val
+            except KeyError as e:
+                model_data[map2] = 0
+
+
+
+
+        model_datas.append(model_data)
+
+print(model_datas)
+
+import pandas as pd
+
+
+df = pd.DataFrame.from_records(model_datas, index="Id")
+
+print(df)
+
+from sklearn.model_selection import train_test_split
+
+train, test = train_test_split(df, test_size=0.2)
+
+print(train)
+print(test)
+
+print(list(train))
+
+X_train = train[['phil_phil', 'phil_phob', 'phob_phob', 'phob_phil', 'energies_phob_phil_mean', 'energies_phob_phob_mean', 'energies_phil_phil_mean', 'energies_phil_phob_mean']]
+Y_train = train[['IC50']]
+Y_train_class = train[['CLASS']]
+
+
+X_test = test[['phil_phil', 'phil_phob', 'phob_phob', 'phob_phil', 'energies_phob_phil_mean', 'energies_phob_phob_mean', 'energies_phil_phil_mean', 'energies_phil_phob_mean']]
+Y_test = test[['IC50']]
+Y_test_class = test[['CLASS']]
+
+# X = df[['phil_phil', 'phil_phob', 'phob_phob', 'phob_phil', 'energies_phob_phil_mean', 'energies_phob_phob_mean', 'energies_phil_phil_mean', 'energies_phil_phob_mean', 'C_in_phil', 'C_in_phob', 'S_in_phil', 'S_in_phob', 'F_in_phil', 'F_in_phob', 'N_in_phil', 'N_in_phob', 'O_in_phil', 'O_in_phob', 'Br_in_phil', 'Br_in_phob', 'Cl_in_phil', 'Cl_in_phob']]
+X = df[['phil_phil', 'phil_phob', 'phob_phob', 'phob_phil', 'energies_phob_phil_mean', 'energies_phob_phob_mean', 'energies_phil_phil_mean', 'energies_phil_phob_mean']]
+y = df['IC50']
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import LinearSVR
+from sklearn.tree import ExtraTreeRegressor
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
+
+# linear = LinearRegression()
+# model = linear.fit(X_train, Y_train)
+#
+svr = LinearSVR()
+# model = svr.fit(X_train, Y_train)
+
+tree = ExtraTreeRegressor()
+model = tree.fit(X_train, Y_train)
+
+from sklearn.tree import DecisionTreeRegressor
+
+regressor = DecisionTreeRegressor(random_state=0)
+
+# tree_c = ExtraTreeClassifier()
+# model_c = tree_c.fit(X_train, Y_train_class)
+from sklearn.model_selection import cross_val_score
+
+print("---CROSS---")
+print(cross_val_score(tree, X, y, cv=4))
+
+# rfr = RandomForestRegressor(max_depth=6, random_state=0)
+# model = rfr.fit(X_train, Y_train)
+
+
+preds_train = model.predict(X_train)
+print("---TRAIN---")
+print(Y_train.values)
+print(preds_train)
+
+print("---TEST---")
+preds_test = model.predict(X_test)
+print(Y_test.values)
+print(preds_test)
+
+# preds_train_c = model_c.predict(X_train)
+# print("---TRAIN---")
+# print(Y_train_class.values)
+# print(preds_train_c)
+
+# print("---TEST---")
+# preds_test_c = model_c.predict(X_test)
+# print(Y_test_class.values)
+# print(preds_test_c)
+
+
+from sklearn.metrics import r2_score
+
+print(r2_score(Y_train, preds_train))
+print(r2_score(Y_test, preds_test))
+
+
 
 # import matplotlib.pyplot as plt
 #
